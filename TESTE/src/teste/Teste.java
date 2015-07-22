6faf29be9770ffa1e3449c304436d28641c0fa31
flavio-barros <junior_barros3@hotmail.com>
2015-07-22 14:09:48 -0300

    minha resposta

--- TESTE/src/teste/Teste.java
+++ TESTE/src/teste/Teste.java
@@ -3,5 +3,6 @@ package teste;
 public class Teste {
     public static void main(String[] args) {        
         System.out.println("1, 2, 3, TESTANDO...");
+        System.out.println("Zé é o car****.");
     }
 }
